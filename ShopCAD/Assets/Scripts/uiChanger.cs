670f3560be573d20cd5d56ef09f8b6df4142df44
Matthew Goedde <mgoedde@bgsu.edu>
2018-03-09 11:11:14 -0500

    Start Button Scripts;

---
+++ ShopCAD/Assets/Scripts/uiChanger.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class uiChanger : MonoBehaviour {
+
+    public Button woodMat;  //the material buttons for each material
+    public Button metalMat;
+    public Button pvcMat;
+
+    public GameObject woodSpPan; //the spawn panel for each material
+    public GameObject metalSpPan;
+    public GameObject pvcSpPan;
+           
+    public GameObject woodToolPan; //the tool panel for each material
+    public GameObject metalToolPan;
+    public GameObject pvcToolPan;
+
+    // Use this for initialization
+    void Start () {
+        woodSpPan.SetActive(true);
+        metalSpPan.SetActive(false);
+        pvcSpPan.SetActive(false);
+
+        woodToolPan.SetActive(true);
+        metalToolPan.SetActive(false);
+        pvcToolPan.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }                                              
+}

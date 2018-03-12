27ad5f50b293803a18c8de0a88734dffc746586e
Matthew Goedde <mgoedde@bgsu.edu>
2018-03-12 16:51:35 -0400

    Finish materials buttons script; T-5,4,3...

--- ShopCAD/Assets/Scripts/uiChanger.cs
+++ ShopCAD/Assets/Scripts/uiChanger.cs
@@ -6,31 +6,81 @@ using UnityEngine.EventSystems;
 
 public class uiChanger : MonoBehaviour {
 
-    public Button woodMat;  //the material buttons for each material
-    public Button metalMat;
-    public Button pvcMat;
+    public Button woodMatObj;  //the material buttons for each material
+    public Button metalMatObj;
+    public Button pvcMatObj;
 
-    public GameObject woodSpPan; //the spawn panel for each material
-    public GameObject metalSpPan;
-    public GameObject pvcSpPan;
-           
-    public GameObject woodToolPan; //the tool panel for each material
-    public GameObject metalToolPan;
-    public GameObject pvcToolPan;
+
+    //the Spawn Buttons and Tool Buttons for each material
+    public GameObject woodSp;
+    public GameObject woodTool;
+
+    public GameObject metalSp;
+    public GameObject metalTool;
+
+    public GameObject pvcSp;
+    public GameObject pvcTool;
+
+
+    public bool woodBool = false;
+    public bool metalBool = false;
+    public bool pvcBool = false;
 
     // Use this for initialization
     void Start () {
-        woodSpPan.SetActive(true);
-        metalSpPan.SetActive(false);
-        pvcSpPan.SetActive(false);
 
-        woodToolPan.SetActive(true);
-        metalToolPan.SetActive(false);
-        pvcToolPan.SetActive(false);
-	}
+        Button woodMat = woodMatObj.GetComponent<Button>();
+        Button metalMat = metalMatObj.GetComponent<Button>();
+        Button pvcMat = pvcMatObj.GetComponent<Button>();
+
+        
+        woodMat.onClick.AddListener(woodSwitch);
+        metalMat.onClick.AddListener(metalSwitch);
+        pvcMat.onClick.AddListener(pvcSwitch);
+
+        woodSp.SetActive(woodBool);
+        woodTool.SetActive(woodBool);
+
+        metalSp.SetActive(metalBool);
+        metalTool.SetActive(metalBool);
+
+        pvcSp.SetActive(pvcBool);
+        pvcTool.SetActive(pvcBool);
+    }
 	
 	// Update is called once per frame
 	void Update () {
+        woodSp.SetActive(woodBool);
+        woodTool.SetActive(woodBool);
 
-    }                                              
+        metalSp.SetActive(metalBool);
+        metalTool.SetActive(metalBool);
+
+        pvcSp.SetActive(pvcBool);
+        pvcTool.SetActive(pvcBool);
+    }
+    
+    void stateSwitch(ref bool selection)
+    {
+        woodBool = false;
+        metalBool = false;
+        pvcBool = false;
+
+        selection = true;
+    }
+
+    void woodSwitch()
+    {
+        stateSwitch(ref woodBool);
+    }
+    
+    void metalSwitch()
+    {
+        stateSwitch(ref metalBool);
+    }
+
+    void pvcSwitch()
+    {
+        stateSwitch(ref pvcBool);
+    }
 }
